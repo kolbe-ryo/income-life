ea467d4b5fb7a3426c29c771349b33c7ce17fe72
kolbe <water.northground@gmail.com>
2022-11-12 16:12:19 +0900

    re-init

---
+++ lib/util/logger.dart
@@ -0,0 +1,9 @@
+import 'package:simple_logger/simple_logger.dart';
+
+final logger = SimpleLogger()
+  ..setLevel(
+    Level.ALL,
+    includeCallerInfo: true, // リリースビルドではfalseにする
+  )
+  ..mode = LoggerMode.log
+  ..formatter = (info) => '${info.level} ${info.time} ${info.message}';
